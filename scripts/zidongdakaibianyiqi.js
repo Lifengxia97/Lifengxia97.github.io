75cb814d7b7cf9bd9b94c87fb535d9281e88ca4c
Lifengxia97 <1024993005@qq.com>
2024-01-02 21:16:39 +0800

    bf

--- scripts/zidongdakaibianyiqi.js
+++ scripts/zidongdakaibianyiqi.js
@@ -3,5 +3,5 @@ var spawn = require('child_process').exec;
 
 // Hexo 3 用户复制这段
 hexo.on('new', function(data){
-  spawn('start  "C:\Program Files\Typora\Typora.exe" ' + data.path);
+  spawn('start "D:\Typora\Typora.exe" ' + data.path);
 });
